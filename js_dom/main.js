281eb3a3a69cd407ccee09931cd926105a4fde6f
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-10-19 21:56:04 +0330

    chore : creating elements

--- js_dom/main.js
+++ js_dom/main.js
@@ -1,25 +1,44 @@
+/** @type {HTMLElement} */
+// ==================== selecting ====================
+// // get element by id
+// const title = document.getElementById('main-heading');
+// console.log(title)
+
+// // get element by class name
+// const classes = document.getElementsByClassName('list-item');
+// console.log(classes)
 
 
-// get element by id
-const title = document.getElementById('main-heading');
-console.log(title)
-
-// get element by class name
-const classes = document.getElementsByClassName('list-item');
-console.log(classes)
+// // get element by class name
+// const sometoher_thing = document.getElementsByTagName('li');
+// console.log(sometoher_thing)
 
 
-// get element by class name
-const sometoher_thing = document.getElementsByTagName('li');
-console.log(sometoher_thing)
+// // queryselector
+// const container = document.querySelector('div')
+// console.log(container)
 
 
-// queryselector
-const container = document.querySelector('div')
-console.log(container)
+// // select all
+// const all_ = document.querySelectorAll('div')
+// console.log(container)
 
 
-// select all
 
-const all_ = document.querySelectorAll('div')
-console.log(container)
+// ==================== styling ====================
+
+// const title = document.querySelector('#main-heading');
+// title.style.backgroundColor = 'red';
+
+// const list_items = document.querySelectorAll('.list-item');
+
+// for (let i = 0; i < list_items.length ; ++i){
+//     list_items[i].style.backgroundColor = 'lightblue';
+// }
+
+// console.log("the title is ; %s " , list_items
+// );
+
+
+// ==================== creating elements ====================
+
