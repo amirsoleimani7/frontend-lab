dca335a828eddec3bf70d11142947993b7b9e79c
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-10-19 21:33:22 +0330

    chore : selectors

---
+++ js_dom/main.js
@@ -0,0 +1,25 @@
+
+
+// get element by id
+const title = document.getElementById('main-heading');
+console.log(title)
+
+// get element by class name
+const classes = document.getElementsByClassName('list-item');
+console.log(classes)
+
+
+// get element by class name
+const sometoher_thing = document.getElementsByTagName('li');
+console.log(sometoher_thing)
+
+
+// queryselector
+const container = document.querySelector('div')
+console.log(container)
+
+
+// select all
+
+const all_ = document.querySelectorAll('div')
+console.log(container)
