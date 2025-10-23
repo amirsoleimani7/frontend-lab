c7d313cfab57d5079cd4a79f7f0f5a702f5901ef
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-10-23 18:28:55 +0330

    add : task add (buttons needed)

--- js_dom/miniproject_5/main.js
+++ js_dom/miniproject_5/main.js
@@ -2,14 +2,28 @@
 
 // vars 
 let current_tasks = document.getElementById('current-task');
-const submit_button = document.querySelector('.submit')
+const submit_button = document.querySelector('.submit');
+let task_list = document.querySelector('.task-list');
+
+function making_task(task_name){
+    
+    let main_div = document.createElement('div');
+    main_div.className = 'task';
+    let p = document.createElement('p');
+    p.textContent = task_name;
+    main_div.appendChild(p);
+    return main_div;
+}
 
 
 submit_button.addEventListener('click' , function(){
-    console.log('User Task is : ' , current_tasks.value);
-    
-    // clearing the value .. 
-    current_tasks.value = ' ';
-})
 
+    console.log('User Task is : ' , current_tasks.value);    
+
+    if (current_tasks.value !== ""){
+        div = making_task(current_tasks.value);
+        current_tasks.value = ' ';
+        task_list.appendChild(div);
+    }
+})
 
