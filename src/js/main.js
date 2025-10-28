17514bf76808923dc97ac267cd06ea26ef02d2b1
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-10-28 22:36:20 +0330

    add : success-card added

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+// vars 
+let user_email_ = document.getElementById('user-email');
+let sign_up_page = document.getElementsByClassName('sign-up');
+let success_page = document.getElementsByClassName('sucesss-card');
+let submit_button = document.querySelectorAll('.submit');
+
+submit_button.addEventListener('click' , function(){
+    sign_up_page.style  
+})
