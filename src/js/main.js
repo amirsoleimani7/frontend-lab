bdf7c73126c84aa65f16d1af7e9dda77509bb596
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-10-29 20:00:51 +0330

    fix :audio fix ..

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 // vars 
-import submit_sound from '../../assets/audio/submit.wav'
-import close_sound from '../../assets/audio/close.mp3'
+// import submit_sound from '../../assets/audio/submit.wav'
+// import close_sound from '../../assets/audio/close.mp3'
 
 let user_email_ = document.getElementById('user-email');
 let form = document.querySelector('.subscription-form');
@@ -16,8 +16,8 @@ let dismiss_button = document.querySelector('.dismiss')
 
 
 // sounds
-let submit_sound = new Audio(submit_sound);
-let close_sound = new Audio(close_sound);
+let submit_sound = new Audio('../../assets/audio/submit.wav');
+let close_sound = new Audio('../../assets/audio/close.mp3');
 
 
 form.addEventListener("submit" , function(e){
