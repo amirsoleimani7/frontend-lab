de52b4a4c16c68859186d23776d73aad557d35b2
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-10-30 23:51:23 +0330

    add: info card append added , img still remaining

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,14 @@ let grid_container = document.querySelector('.grid-container');
 let current_category_type = 'Daily';
 
 
-
+// ```
+//     - Orange 300 (work): hsl(15, 100%, 70%)
+//     - Blue 300 (play): hsl(195, 74%, 62%)
+//     - Pink 400 (study): hsl(348, 100%, 68%)
+//     - Green 400 (exercise): hsl(145, 58%, 55%)
+//     - Purple 700 (social): hsl(264, 64%, 52%)
+//     - Yellow 300 (self care): hsl(43, 84%, 65%)
+// ```
 
 
 for (let i = 0; i < radio_button_form.length ; ++i){
@@ -21,15 +28,62 @@ for (let i = 0; i < radio_button_form.length ; ++i){
 
 function update_card(info_json){
     // making the html .card and adding to the grid
+    console.log(grid_container.children);
+    
+    for (let i = grid_container.childElementCount -1 ; i>= 0; i-- ){
+        if (grid_container.children[i].className == 'card'){
+            grid_container.removeChild(grid_container.children[i]);
+        }
+    }
+
+    
+    console.log(grid_container.children);
+    
     for (let i = 0 ; i < info_json.length ;++i){
+        
         let title = info_json[i].title;
         let current_time = info_json[i].current_time;
         let prev_time = info_json[i].previous_time;
-
         
+        let card_div = document.createElement('div');
+        card_div.classList.add('card');
+        
+        let detail_div = document.createElement('div');
+        detail_div.classList.add('detail');
+        
+        let header = document.createElement('h1');
+        header.innerText = title;
+        header.classList.add('item-title');
 
+        let first_span = document.createElement('span');
+        first_span.innerText = `${current_time}hrs`;
+        first_span.classList.add('current');
+
+        let second_span = document.createElement('span');
+        
+        switch (current_category_type){
+            case "Daily": 
+                second_span.innerText = `Last Day - ${prev_time}hrs`;
+                break;
+            case "Weekly":
+                second_span.innerText = `Last Week - ${prev_time}hrs`;
+                break;
+            case "Monthly":
+                second_span.innerText = `Last Month - ${prev_time}hrs`;
+                break;
+        }
+        
+        second_span.classList.add('prev');
+        
+        detail_div.appendChild(header);
+        detail_div.appendChild(first_span);
+        detail_div.appendChild(second_span);
+        
+        card_div.appendChild(detail_div);
+        grid_container.appendChild(card_div);
     }
 }
+
 
 
 async function update_html_category(){
@@ -55,6 +109,7 @@ async function update_html_category(){
             }
             current_json.push(current_item);
         }
+        console.log(`current category type is : ${current_category_type}`);
         update_card(current_json);
     } 
     
