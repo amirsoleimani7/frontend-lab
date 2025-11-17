bf9459df3e99e3f56276f3f5f1c5b862f72caf96
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-11-17 10:21:32 +0330

    fix : no rating error fixed

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,5 @@
 let rating_card = document.querySelector('.rating-container');
-
-
+let is_cheacked = false;
 
 let submit_button = document.querySelector('.submit');
 let radio_buttons = document.querySelectorAll('input[name=rating]');
@@ -13,12 +12,20 @@ let tnq_span_ = document.getElementById('user-rating');
 
 submit_button.addEventListener('click' ,(e)=>{
     console.log(`we are in the event`);    
+    
     for ( let i = 0 ; i < radio_buttons.length ;++i ){
         if(radio_buttons[i].checked){
             tnq_span_.innerText = radio_buttons[i].value;
+            is_cheacked = true;
         }
     }
+    
+    if (is_cheacked){
+        rating_card.style.display = 'none';
+        tnq_card.style.display = 'flex';
+    }
 
-    rating_card.style.display = 'none';
-    tnq_card.style.display = 'flex';
+    else{
+        submit_button.style.backgroundColor = 'red';   
+    }      
 })
