bad724a3543af1b1923c25476f7936877ea44ec6
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-11-22 18:32:32 +0330

    fix : fixed refreshing after submit in firefox

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,10 @@
 let succ_msg = document.querySelector('.success-message');
-succ_msg.classList.add('open');
+let submit_button = document.querySelector('.submit-button');
+
+submit_button.addEventListener('click' , (e)=> {
+    e.preventDefault();
+    succ_msg.style.display = getComputedStyle(succ_msg).display == 'none' ? 'block' : 'none';
+})
+
 
  
