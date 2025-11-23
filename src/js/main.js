b9ab388c4624f31b48370d32ef5132514b40656a
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-11-23 16:51:16 +0330

    add : added naive validations

--- src/js/main.js
+++ src/js/main.js
@@ -14,34 +14,57 @@ let consent = document.querySelector('#consent');
 // errors
 let radio_error = document.querySelector('#que-type-error');
 let consent_error = document.querySelector('#consent-error');
+let first_name_error = document.querySelector('#first-name-error');
+let last_name_error = document.querySelector('#last-name-error');
+let email_empty_error = document.querySelector('#email-error-empty');
+let email_empty_validation = document.querySelector('#email-error-validation');
+let message_error = document.querySelector('#message-error');
 
-
-
-
-let valid_flag = false;
+let valid_flag = true;
 
 function radio_check(radio_in){
     let selected_value = ''
-    for (let i = 0; i < querty_type_radio.length ;++i ){
-        if (querty_type_radio[i].checked){
-            selected_value = querty_type_radio[i].value;
-            // console.log(`selected value is : ${querty_type_radio[i].value}`);
-            // is_checked = true;
+    for (let i = 0; i < radio_in.length ;++i ){
+        if (radio_in[i].checked){
+            selected_value = radio_in[i].value;
         }
-    } 
-    
+    }     
     return selected_value;
 }
 
-
-
 submit_button.addEventListener('click' , (e)=> {
     e.preventDefault();
-    
-    console.log(`first name is : ${first_name.value}`);
-    console.log(`last name is : ${last_name.value}`);
-    console.log(`messsage is : ${messsage.value}`);
 
+    console.log(`messsage is : ${messsage.value}`);
+    // check first_name and last_name
+    if(first_name.value){
+        console.log(`first name is : ${first_name.value}`);
+        first_name_error.style.display = 'none';
+    }
+    else{
+        first_name_error.style.display = 'block';
+        valid_flag = false;
+    }
+
+    if(last_name.value){
+        console.log(`last name is : ${last_name.value}`);
+        last_name_error.style.display = 'none';
+    }
+    else{
+        last_name_error.style.display = 'block'
+        valid_flag = false;
+    }
+    
+    // message_check 
+    if(messsage.value == ''){
+        console.log(`last name is : ${messsage.value}`);
+        message_error.style.display = 'block';
+        valid_flag = false;
+
+    }
+    else{        
+        message_error.style.display = 'none';
+    }
     
     // radio check :
     if (radio_check(querty_type_radio)){
@@ -50,12 +73,13 @@ submit_button.addEventListener('click' , (e)=> {
     }
     else{
         radio_error.style.display = 'block';   
+        valid_flag = false;
     }
-    
-    
 
+    // conseent check
     if (!consent.checked){
         consent_error.style.display = 'block';
+        valid_flag = false;
     }
     else{        
         console.log(`consent  is : ${consent.checked}`);
@@ -63,6 +87,7 @@ submit_button.addEventListener('click' , (e)=> {
     }
 
     if(valid_flag){
+        
         succ_msg.style.display = getComputedStyle(succ_msg).display == 'none' ? 'block' : 'none';
     }
 })
