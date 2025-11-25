26108393f388181e636712066bd6a821e2933602
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-11-25 15:06:42 +0330

    add : added the open navigtion

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,13 @@
 let open_nav_btn = document.querySelector('#open-button');
 let close_nav_btn = document.querySelector('#close-button');
-let 
+let hidden_menu = document.querySelector('.hidden-menu');
 
 console.log(`open button is : ${open_nav_btn}`);
 console.log(`close button is : ${close_nav_btn}`);
 
 
 open_nav_btn.addEventListener('click' , (e)=> {
-    
+    open_nav_btn.style.display = 'none';
+    close_nav_btn.style.display = 'block';
+    hidden_menu.style.maxWidth = '500px';
 })
