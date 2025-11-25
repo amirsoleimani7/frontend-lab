691bb4110c9951fd78126b64c9d1fce5aecf14b3
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-11-25 15:02:54 +0330

    add : added the hidden navigation, event still not added yet

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 let open_nav_btn = document.querySelector('#open-button');
 let close_nav_btn = document.querySelector('#close-button');
+let 
 
 console.log(`open button is : ${open_nav_btn}`);
 console.log(`close button is : ${close_nav_btn}`);
-
 
 
 open_nav_btn.addEventListener('click' , (e)=> {
