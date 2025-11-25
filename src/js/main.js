9147be925b587ccad60caa4e3cb00df194b2d3bc
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-11-25 14:41:32 +0330

    add : added event to open_nav_button

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+let open_nav_btn = document.querySelector('#open-button');
+let close_nav_btn = document.querySelector('#close-button');
+
+console.log(`open button is : ${open_nav_btn}`);
+console.log(`close button is : ${close_nav_btn}`);
+
+
+
+open_nav_btn.addEventListener('click' , (e)=> {
+    
+})
