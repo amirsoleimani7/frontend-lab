27ae115eab8d60302dd14bfbb0d800c0f3f17a0e
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-12-01 09:11:42 +0330

    fix : fixed menu for phone

--- news-homepage-main/src/js/main.js
+++ news-homepage-main/src/js/main.js
@@ -8,7 +8,9 @@ open_nav_btn.addEventListener('click' , (e)=> {
     open_nav_btn.style.display = 'none';
     close_nav_btn.style.display = 'block';
     hidden_menu.style.maxWidth = '500px';
+    hidden_menu.style.position ='fixed';
     main_container.classList.add('x');
+    
 })
 
 
@@ -16,6 +18,7 @@ close_nav_btn.addEventListener('click' , (e)=> {
     open_nav_btn.style.display = 'block';
     close_nav_btn.style.display = 'none';
     hidden_menu.style.maxWidth = '0px';
+    hidden_menu.style.position = '';
     main_container.classList.remove('x');
 })
 
