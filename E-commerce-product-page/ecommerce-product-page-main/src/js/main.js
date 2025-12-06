2d721f046fca3ce28281fe9dc1865d8b47f4002f
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-12-06 10:43:43 +0330

    feat : reading from add-to-cart in js

---
+++ E-commerce-product-page/ecommerce-product-page-main/src/js/main.js
@@ -0,0 +1,12 @@
+// add to cart functionality 
+
+let add_to_cart_button = document.querySelector('#add-to-cart');
+
+
+
+add_to_cart_button.addEventListener('click' , () =>{
+    console.log('add to cart pressed !');
+})
+
+
+
