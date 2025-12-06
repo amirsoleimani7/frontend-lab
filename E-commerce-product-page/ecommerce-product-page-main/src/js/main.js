4879e788c2408b429c82b9bfe9a91775b837a39c
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-12-06 11:12:19 +0330

    add : styling to curent number of items on cart , sltying of it

--- E-commerce-product-page/ecommerce-product-page-main/src/js/main.js
+++ E-commerce-product-page/ecommerce-product-page-main/src/js/main.js
@@ -1,23 +1,10 @@
-// add to cart functionality 
-
-let add_to_cart_button = document.querySelector('#add-to-cart');
-
-
-
-add_to_cart_button.addEventListener('click' , () =>{
-    console.log('add to cart pressed !');
-})
-
-
-
-
+// initialization of the thing
 
 // adding and removing item 
 let add_item_button = document.querySelector('#add-item');
 let remove_item_button = document.querySelector('#remove-item');
 let item_number = document.querySelector('#items-no');
 
-// initialization of the thing
 item_number.textContent = 0;
 
 
@@ -34,5 +21,24 @@ remove_item_button.addEventListener('click' , () => {
         item_number.textContent = parseInt(item_number.textContent) + -1;
     }
 })
+
+
+
+// add to cart functionality 
+let add_to_cart_button = document.querySelector('#add-to-cart');
+let number_of_items_on_cart = document.querySelector('.current-number-of-items');
+
+add_to_cart_button.addEventListener('click' , () =>{
+    if (parseInt(item_number.textContent) != 0){
+        number_of_items_on_cart.style.display = 'block';
+        number_of_items_on_cart.textContent = parseInt(item_number.textContent);
+    }
+    else{        
+        number_of_items_on_cart.style.display = 'none';
+    }
+})    
+
+
+
 
 
