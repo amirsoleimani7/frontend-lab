7c143c297f4b5b7527ff52872b38685d9e7e641f
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-12-06 10:54:01 +0330

    feat : responsive adding and removing items

--- E-commerce-product-page/ecommerce-product-page-main/src/js/main.js
+++ E-commerce-product-page/ecommerce-product-page-main/src/js/main.js
@@ -10,3 +10,29 @@ add_to_cart_button.addEventListener('click' , () =>{
 
 
 
+
+
+// adding and removing item 
+let add_item_button = document.querySelector('#add-item');
+let remove_item_button = document.querySelector('#remove-item');
+let item_number = document.querySelector('#items-no');
+
+// initialization of the thing
+item_number.textContent = 0;
+
+
+// adding an item
+add_item_button.addEventListener('click' , () => {
+    console.log(`current number is : ${item_number.textContent}`);
+    item_number.textContent = parseInt(item_number.textContent) + 1;
+
+})
+
+// removing an item
+remove_item_button.addEventListener('click' , () => {
+    if (parseInt(item_number.textContent) > 0){
+        item_number.textContent = parseInt(item_number.textContent) + -1;
+    }
+})
+
+
