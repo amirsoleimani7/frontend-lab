b8aa99b68b2f7e560dc0efc5fc2701e078451dd2
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-12-14 19:42:41 +0330

    add : spinning cube tested

--- blender-threejs/portfolio/main.js
+++ blender-threejs/portfolio/main.js
@@ -1,1 +1,28 @@
 import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75 , window.innerWidth / window.innerHeight , 0.1 , 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth , window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// making the cube 
+const geometry = new THREE.BoxGeometry(1, 5,1);
+const material = new THREE.MeshBasicMaterial({color : 0x810818 , wireframe : false});
+const cube = new THREE.Mesh(geometry,  material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+
+function animate(){
+    cube.rotation.x += .1;
+    cube.rotation.y += .1;
+    
+    camera.position.z += .1;
+    renderer.render(scene , camera);
+}
+
+renderer.setAnimationLoop( animate );
+
