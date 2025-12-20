77062caccdb60a06352cbe966b84e6eb59563997
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-12-20 22:56:35 +0330

    feat : connection between menu and links

--- loopstudios-landing-page/loopstudios-landing-page-main/src/js/main.js
+++ loopstudios-landing-page/loopstudios-landing-page-main/src/js/main.js
@@ -1,16 +1,19 @@
 //  open and close menu button
 let show_list = document.querySelector('#show-list');
 let close_list = document.querySelector('#close-list');
+let links = document.querySelector('.links');
 
 
 show_list.addEventListener('click' , () => {
     show_list.classList.remove('active');
     close_list.classList.add('active');
+    links.classList.add('hidden');
 })
 
 
 close_list.addEventListener('click' , () => {
     show_list.classList.add('active');
     close_list.classList.remove('active');
+    links.classList.remove('hidden');
 })
 
