a6c26d94970ba904ddcaff80729178037799866c
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-12-20 23:46:48 +0330

    fix : hidden navigation scroll fix

--- loopstudios-landing-page/loopstudios-landing-page-main/src/js/main.js
+++ loopstudios-landing-page/loopstudios-landing-page-main/src/js/main.js
@@ -2,12 +2,14 @@
 let show_list = document.querySelector('#show-list');
 let close_list = document.querySelector('#close-list');
 let links = document.querySelector('.links');
+let main_navigation = document.querySelector('.main-navigation');
 
 
 show_list.addEventListener('click' , () => {
     show_list.classList.remove('active');
     close_list.classList.add('active');
     links.classList.add('hidden');
+    main_navigation.style.position = 'fixed';
 })
 
 
@@ -15,5 +17,7 @@ close_list.addEventListener('click' , () => {
     show_list.classList.add('active');
     close_list.classList.remove('active');
     links.classList.remove('hidden');
+    main_navigation.style.position = '';
 })
+
 
