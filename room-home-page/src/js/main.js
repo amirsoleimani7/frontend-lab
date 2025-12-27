e7c6273cfd073239bd9cd40530d1f96dd76cacdf
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2025-12-27 23:46:53 +0330

    style : left and right buttons added

---
+++ room-home-page/src/js/main.js
@@ -0,0 +1,12 @@
+let left_button = document.querySelector('#left');
+let right_button = document.querySelector('#right');
+
+
+left_button.addEventListener('click' , ()=> {
+    console.log(' : left');    
+})
+
+right_button.addEventListener('click' , ()=> {
+    console.log(' : right');    
+})
+
