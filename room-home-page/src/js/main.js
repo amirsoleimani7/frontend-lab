7cd779c2181115b75ff83326fb52d08a5932415c
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-01-05 22:02:49 +0330

    add : change text with image as well

--- room-home-page/src/js/main.js
+++ room-home-page/src/js/main.js
@@ -5,14 +5,26 @@ let target_image = document.querySelector('#target-image');
 
 let current_pic_index = 1;
 
+function set_num(index){
+    document.querySelector(`#item-${index}`).style.display = 'initial';
+    for (let i = 1; i <= 3 ;++i){
+        if (i != index){
+            document.querySelector(`#item-${i}`).style.display = 'none';    
+        }
+    }    
+}
+
 left_button.addEventListener('click' , ()=> {
     current_pic_index = ( current_pic_index == 1) ? 3 : current_pic_index -1;
     target_image.src = `../../images/desktop-image-hero-${current_pic_index}.jpg`; 
+    set_num(current_pic_index);
 })
 
 right_button.addEventListener('click' , ()=> {
     current_pic_index = ( current_pic_index == 3) ? 1 : current_pic_index +1;        
     target_image.src = `../../images/desktop-image-hero-${current_pic_index}.jpg`; 
+    set_num(current_pic_index);
+
 })
 
 
