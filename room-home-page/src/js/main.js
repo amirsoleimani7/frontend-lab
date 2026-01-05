6dc9aff8b5f2e15febcb0266c137be32695c9aa4
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-01-05 21:08:11 +0330

    add: image changing functionality added

--- room-home-page/src/js/main.js
+++ room-home-page/src/js/main.js
@@ -1,19 +1,18 @@
 let left_button = document.querySelector('#left');
 let right_button = document.querySelector('#right');
+let target_image = document.querySelector('#target-image');
 
 
 let current_pic_index = 1;
 
 left_button.addEventListener('click' , ()=> {
-    current_pic_index = ( current_pic_index == 1) ? 3 : current_pic_index -1;        
-    
-
+    current_pic_index = ( current_pic_index == 1) ? 3 : current_pic_index -1;
+    target_image.src = `../../images/desktop-image-hero-${current_pic_index}.jpg`; 
 })
 
 right_button.addEventListener('click' , ()=> {
     current_pic_index = ( current_pic_index == 3) ? 1 : current_pic_index +1;        
-    console.log(`current index is : ${current_pic_index}`);
-    
+    target_image.src = `../../images/desktop-image-hero-${current_pic_index}.jpg`; 
 })
 
 
