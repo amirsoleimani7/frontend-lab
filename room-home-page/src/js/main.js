03223d38470086ba08da329361f911c6038eb5de
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-01-05 21:04:15 +0330

    add : index functionality added

--- room-home-page/src/js/main.js
+++ room-home-page/src/js/main.js
@@ -2,11 +2,18 @@ let left_button = document.querySelector('#left');
 let right_button = document.querySelector('#right');
 
 
+let current_pic_index = 1;
+
 left_button.addEventListener('click' , ()=> {
-    console.log(' : left');    
+    current_pic_index = ( current_pic_index == 1) ? 3 : current_pic_index -1;        
+    
+
 })
 
 right_button.addEventListener('click' , ()=> {
-    console.log(' : right');    
+    current_pic_index = ( current_pic_index == 3) ? 1 : current_pic_index +1;        
+    console.log(`current index is : ${current_pic_index}`);
+    
 })
+
 
